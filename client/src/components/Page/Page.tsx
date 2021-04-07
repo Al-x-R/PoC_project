20183e6e37affb63d78beae473fa1d38b085df36
alexander <alexander.roshchin85@gmail.com>
2021-04-07 16:02:30 +0300

    Add  create text dialog

--- client/src/components/Page/Page.tsx
+++ client/src/components/Page/Page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Paper from '@material-ui/core/Paper';
 import Picture from "../Picture/Picture";
-import TextCreate from "../Text/TextCreate";
 
 const paperStyle = {
     width: '500px',
